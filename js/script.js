c089dbda750e2f2c1d0400f39915b29a422575cc
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-03-25 17:44:57 +0100

    Fix: age and final message

--- js/script.js
+++ js/script.js
@@ -16,14 +16,18 @@ console.log(age, typeof age)
 //PARTE LOGICA
 //Dichiaro la variabile (Se meti 'const' non la puoi cambiare e fare i calcoli e serve per forza 'let')
 let ticket;
+let discount;
 
 //Calcolo il prezzo del biglietto + aggiungo lo sconto in base all'età
-if (age < 17) {
+if (age < 18) {
     ticket = (0.21 * kiloMetri) - (0.21 * kiloMetri * 0.2);
-} else if (age >= 18 && age < 64) {
+    discount = "sei minorenne";
+} else if (age >= 18 && age < 65) {
     ticket = (0.21 * kiloMetri);
-} else if (age > 64) {
+    discount = "non ti è stato applicato lo sconto"
+} else if (age >= 65) {
     ticket = (0.21 * kiloMetri) - (0.21 * kiloMetri * 0.4);
+    discount = " sei over 65";
 }
 
 console.log(ticket)
@@ -34,7 +38,7 @@ console.log(ticket1)
 
 // OUTPUT
 // Preparo il messaggio
-const resultMessage = `Il Biglietto costa: ${ticket1} €`;
+const resultMessage = `Il Biglietto costa: ${ticket1} € perché ${discount}.`;
 
 // Stampo il risultato della somma in pagina
 document.getElementById("result").innerHTML = resultMessage;
